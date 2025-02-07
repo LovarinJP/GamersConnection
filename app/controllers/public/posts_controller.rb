0eaff22b2e40a9947081f2ef5b732f1b1c60fe91
LovarinJP <187247562+LovarinJP@users.noreply.github.com>
2025-02-07 03:03:38 +0000

    pre-complete

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -1,5 +1,6 @@
 class Public::PostsController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:index]
+  before_action :set_editable, only: [:show, :edit]
 
   def new
     @post = Post.new
@@ -21,12 +22,25 @@ class Public::PostsController < ApplicationController
   end
 
   def edit
+    @post = Post.find(params[:id])
   end
 
   def update
+    @post = Post.find(params[:id])
+    @post.update(post_params)
+    redirect_to post_path(@post)
   end
 
   def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to mypage_path
+  end
+
+
+  def set_editable
+    @post = Post.find(params[:id])
+    @editable = (current_user == @post.user)
   end
 
   private
