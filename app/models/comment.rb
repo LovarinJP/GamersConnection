86d3b78a92b4cd296304c0cb2b7d952d6e2d1795
LovarinJP <187247562+LovarinJP@users.noreply.github.com>
2025-02-11 06:28:58 +0000

    add feature/comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,14 @@
 class Comment < ApplicationRecord
+  has_one_attached :image
+
   belongs_to :post
   belongs_to :user
+
+  validates :comment, presence: true, length: { maximum: 200 }
+
+  def get_image(width, height)
+    if image.attached?
+      image.variant(resize_to_limit: [width, height]).processed
+    end
+  end
 end
