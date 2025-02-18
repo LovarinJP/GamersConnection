b0f9691039c1c9240134e1a953cdafe90319a901
LovarinJP <187247562+LovarinJP@users.noreply.github.com>
2025-02-18 13:29:03 +0000

    adding index of DM

--- app/controllers/public/rooms_controller.rb
+++ app/controllers/public/rooms_controller.rb
@@ -2,6 +2,11 @@ class Public::RoomsController < ApplicationController
   def create
   end
 
+  def index
+    my_room_id = current_user.entries.pluck(:room_id)
+    @another_emtries = Entri.where(room_id: my_room_id).where.not(user_id: current_user.id)
+  end
+
   def show
   end
 end
