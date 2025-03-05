bb6e8320381167b704ae930dcf015d2e2df2762c
LovarinJP <187247562+LovarinJP@users.noreply.github.com>
2025-03-05 04:56:28 +0000

    add testdata

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,11 +11,53 @@ Admin.create(
   password: "password"
 )
 
+image_path = Rails.root.join('app', 'assets', 'images', 'no_image.jpg')
+
 User.create!(
   [
+    {
     email: "guest@example.com",
     password: Devise.friendly_token[0, 20],
     name: "ゲストユーザー",
     nickname: "ゲスト"
+    },
+
+    {
+    email: "test@gmail.com",
+    password: "aaaaaa",
+    name: "test",
+    nickname: "test",
+    profile_image: { io: File.open(image_path), filename: 'no-image.jpg', content_type: 'image/jpeg'}
+    }
+]
+)
+
+Post.create(
+  [
+    {
+    user_id: 1,
+    body: "これはテスト投稿です"
+    },
+
+    {
+    user_id: 2,
+    body: "ゲームについていろいろ話したい！"
+    }
+  ]
+)
+
+Group.create(
+  [
+    {
+    name: "test group",
+    owner_id: 1,
+    caption: "これはテストグループです"
+    },
+
+    {
+    name: "Gamers",
+    owner_id: 2,
+    caption: "ゲーム好き集まれー！"
+    }
   ]
 )
